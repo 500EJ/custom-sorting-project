922f8bc6ec3a6802d0f52e662912550f4ba8eadf
Ethan Julius <56324629+500EJ@users.noreply.github.com>
2023-02-07 17:41:38 -0500

    Implement oddEvenSort

--- custom-sorts.ts
+++ custom-sorts.ts
@@ -11,8 +11,19 @@ export function ageSort(users: User[]): User[] {
   return users.sort((a, b) => (a.age < b.age ? -1 : a.age > b.age ? 1 : 0));
 }
 
-export function oddEvenSort(arr) {
-  // Your code here
+export function oddEvenSort(arr: number[]): number[] {
+  return arr.sort((a, b) => {
+    if (a % 2 === 1 && b % 2 === 0) {
+      return -1;
+    } else if (a % 2 === 0 && b % 2 === 1) {
+      return 1;
+    } else if (a < b) {
+      return -1;
+    } else if (a > b) {
+      return 1;
+    }
+    return 0;
+  });
 }
 
 export function validAnagrams(s, t) {
